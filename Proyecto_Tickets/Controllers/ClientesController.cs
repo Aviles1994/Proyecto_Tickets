3c2122bfd9b89cf2713b5b512946d1093488ccc5
Aviles1994 <gaviles011@accitesz.com>
2019-10-30 17:45:46 -0600

    INSERT

--- Proyecto_Tickets/Controllers/ClientesController.cs
+++ Proyecto_Tickets/Controllers/ClientesController.cs
@@ -4,17 +4,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_Tickets.Models;
+using Proyecto_Tickets.Models.ViewsModels;
 
 namespace Proyecto_Tickets.Controllers
 {
     public class ClientesController : Controller
     {
-        // GET: Clientes
-        public ActionResult CrearCliente()
-        {
+        public ActionResult CrearCliente() {
             ViewData["nombre_user"] = UserSession.nombre_user;
+            List<Entidad_federativaViewModel> lst = null;
+            using (Sistema_TicketsEntities db = new Sistema_TicketsEntities())
+            {
+                lst =
+                  (from d in db.Entidad_Federativa
+                   select new Entidad_federativaViewModel
+                   {
+                       ID_Entidad = d.ID_Entidad_Federativa,
+                       Nombre = d.Nombre_Entidad_Federativa
+
+                   }).ToList();
+            }
+
+            List<SelectListItem> items = lst.ConvertAll(d =>
+            {
+                return new SelectListItem()
+                {
+                    Text = d.Nombre.ToString(),
+                    Value = d.ID_Entidad.ToString(),
+                    Selected = false
+                };
+
+            });
+
+
+
+
+            ViewBag.items = items;
+
             return View();
         }
+
+       [HttpPost]
+        public ActionResult CrearCliente(ClientesViewsModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new Sistema_TicketsEntities())
+            {
+                Cliente clientes = new Cliente();
+                clientes.Nombre_Cliente = model.Nombre_Cliente;
+                clientes.Calle = model.Calle;
+                clientes.Numero = model.Numero;
+                clientes.Colonia = model.Colonia;
+                clientes.Telefono = model.Telefono;
+                clientes.Correo_Electronico = model.Correo_Electronico;
+                clientes.ID_Entidad_Federativa = model.ID_Entidad_Federativa;
+
+                db.Cliente.Add(clientes);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception err)
+                {
+
+                    throw new Exception(err.Message);
+                }
+                
+
+                
+
+            }
+            return Redirect(Url.Content("~/Views/Clientes/BuscarCliente/"));
+
+    }
 
         public ActionResult BuscarCliente()
         {
---
+++ Proyecto_Tickets/Models/ViewsModels/Entidad_federativaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Tickets.Models.ViewsModels
+{
+    public class Entidad_federativaViewModel
+    {
+        public int ID_Entidad { get; set; }
+        public string Nombre { get; set; }
+    }
+}
