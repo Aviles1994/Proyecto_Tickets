1b7cac51165e5577c51b38650c23d10cf6837a92
Aviles1994 <gaviles011@accitesz.com>
2019-11-05 18:57:28 -0600

    ayuda

--- Proyecto_Tickets/Controllers/TicketsController.cs
+++ Proyecto_Tickets/Controllers/TicketsController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Proyecto_Tickets.Models.Viewslist;
+using Proyecto_Tickets.Models;
+using System.Collections;
 
 namespace Proyecto_Tickets.Controllers
 {
@@ -13,6 +16,8 @@ namespace Proyecto_Tickets.Controllers
         public ActionResult Add_Ticket()
         {
             ViewData["nombre_user"] = UserSession.nombre_user;
+            llenarListaMedioContacto();
+            llenarListaServicios();
             return View();
         }
 
@@ -21,5 +26,58 @@ namespace Proyecto_Tickets.Controllers
             ViewData["nombre_user"] = UserSession.nombre_user;
             return View();
         }
+
+        public void llenarListaServicios()
+        {
+            List<listServico> lst = null;
+            using (Sistema_TicketsEntities db = new Sistema_TicketsEntities())
+            {
+                lst =
+                  (from d in db.Servicio
+                   select new listServico
+                   {
+                       idservicio = d.ID_Servicio,
+                       nameservicio = d.Nombre_Servicio
+                   }).ToList();
+            }
+            List<SelectListItem> items = lst.ConvertAll(d =>
+            {
+                return new SelectListItem()
+                {
+                    Text = d.nameservicio.ToString(),
+                    Value = d.idservicio.ToString(),
+                    Selected = false
+                };
+
+            });
+            ViewBag.items = items;
+        }
+
+        public void llenarListaMedioContacto()
+        {
+            List<listMedioContacto> lst = null;
+            using (Sistema_TicketsEntities db = new Sistema_TicketsEntities())
+            {
+                lst =
+                  (from d in db.Medio_de_Contacto
+                   select new listMedioContacto
+                   {
+                       idmedio = d.ID_Medio_de_Contacto,
+                       namemedio = d.Nombre_MedioC
+                   }).ToList();
+            }
+            List<SelectListItem> items = lst.ConvertAll(d =>
+            {
+                return new SelectListItem()
+                {
+                    Text = d.namemedio.ToString(),
+                    Value = d.idmedio.ToString(),
+                    Selected = false
+                };
+
+            });
+            ViewBag.items = items;
+        }
+
     }
 }
---
+++ Proyecto_Tickets/Models/Viewslist/lisMedioContacto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_Tickets.Models.Viewslist
+{
+    public class listMedioContacto
+    {
+        public int idmedio { get; set; }
+        public string namemedio { get; set; }
+    }
+}
